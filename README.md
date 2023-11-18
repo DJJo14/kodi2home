27b73681a4c915a0238c491d5278364afb2ddc81
DJJo14 <DJJo14@gmail.com>
2023-11-18 10:01:17 +0100

    update readme and re-writen code (#5)
    * black code style
    
    * edit comments
    
    * new whay of code
    
    * make docker shorder
    
    * debug code
    
    * code tested now change version number
    
    * change sentence
    
    * change into
    
    * Update README.md
    
    * add images and change text
    
    * add youtube video
    
    * Add hardpart
    
    * update readme
    
    * add readme
    
    * add images

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
-kodi2home
+Kodi2home
 =========
-kodi calling Home Assistant
+In home assistant you can connect to Kodi and control Kodi from Home assistant, but you cannot trigger an action when pressing a button on Kodi. This is where Kodi2home comes in to play. Kodi2home connects to the websocket from Kodi and to the websocket from home assistant. When it receives a "notifyall" from Kodi, it will trigger an automation on home assistant. In Kodi you change the keymap so that when you press a button it wil send the "notifyall" message. This whay you can turn on a light with the remote from Kodi. 
+```mermaid
+flowchart LR
+    A["Remote"] -->|usb| B(Kodi)
+    B --> |NotifyAll| C{Kodi2Home}
+    C -->|trigger automation| D[Home Assistant]
+    D --> E[action]
+```
+Note: When you change a button in the keymap of Kodi, it will override the existing function of that button. But you can control Kodi in your home assistant automation, and still do the same action. See [Example](#example)
 
-Install by going to Supervisor -> Add-on store -> Add new repository by url and fill in `https://github.com/DJJo14/kodi2home`.
+# Install
 
-Example config for the add-on:
+You Need the following things to make this work:
+ - Home assitant
+ - Kodi
+ - A Usb remote connected to kodi <BR>(IR or Bluetooth problobly possible but not tested)
+ - A whay to change the keymap on kodi <BR>
+   (example: ssh in to libreelec or notepad on windows)
+   
+Install kodi2home by going to Settings -> Addon's -> Add-on store -> Vertical ... (upperright) -> repositories -> fill in the url: `https://github.com/DJJo14/kodi2home`.
+After a refresh the Kodi2home is now in you add-on store. click on it and press install. After installing you must configurate the add-on the configuration tab.
+Example of the configuration:
 ```yaml
 kodi_adress: youre kodi ip adress
 kodi_http_port: 8080
@@ -15,32 +32,56 @@ home_adress: 'ws://supervisor/core/api/websocket'
 home_ssl: false
 ```
 
-home_adress and home_ssl you not need to change when your are using it in hassio
+If you are using hassio you do **not** need to change the home_adress and home_ssl. The values are already set to the add-on values.
 
+You need to change the keymap on kodi. [More info about the keymap.](https://kodi.wiki/view/HOW-TO:Modify_keymaps) There is a "Add-on:Keymap Editor" but you can not set it to send the NotifyAll message. So you need to change it in the keyboard.xml (or other .xml name)
 put somting like this in your keymap of kodi:
 ```xml
 <volume_up>NotifyAll("kodi2home", "kodi_call_home", {"trigger":"automation.volume_up"})</volume_up>
 ```
-add "automation.volume_up" will be triggert, when pressing volume_up.
-
+Add an automation on home assistant with the id/name: "volume_up".(you can leave automation triggers empty) Start up the kodi2home add-on and press the volume_up button. The automation will be triggerd on Home Assistant. Home Assistant run you action that you put in you automation.
 
 ## Why
-This is done so you can easy call services at home assistant, the other way around was already possible but, this is still a missing feature of Home Assistant.
+Home assitant can control kodi, but when you press a button on kodi you can not trigger a action on home assistant. Kodi2home is a man-in-the-middel. I found this a missing feature of Home assistent. This way you can turn on a light with a kodi remote. Or change the volume on your amplifier instead of kodi. Or much smarter things.
 
 ## Why this way
-With Kodi you can call scripts and with that do the same, because it has to start the script and then connect, it can be a sec later before the action is done.
+With Kodi you can call scripts and with that do the same. For example call a script that calls a webtrigger. Because it has to start the script and then connect, it can be a sec later before the action is done. I created a video to see the difference. Watch closely to number change. Volume up is kodi2home and down is a script.
+[![Call a script vs kodi2home](http://img.youtube.com/vi/MlcBf1nm40w/0.jpg)](http://www.youtube.com/watch?v=MlcBf1nm40w)
+
+## NotifyAll?
+The hard part is to send out a message from kodi, without calling a script. Because calling a script from kodi takes almost 1 sec. This is where the NotifyAll comes in, this is a buildin message from kodi sending to all of the connected (web-)sockets. 
 
 ## How is it done
-Kodi2home is not more than two websocket's connected to etch other. One to code lisening to the "NotifyAll" and the other one sending it to Home Assistant. Both of the websockets are already connected.
-It is done in a way Home Assistant uses kodi, so if it is liked, it can be intergated in to Home Assistant kodi intergration.
+Kodi2home is not more than two websockets connected to each other. One side is lisening for the "NotifyAll" and the other side sending it to Home Assistant. Both of the websockets are already connected. So there is less delay there. 
+The script is programed just like Home Assistant kodi intergration, so if it is liked, it can be intergated in to Home Assistant kodi intergration.
 
 ### Reload keymap
-When you start the addon the keymap of kodi gets reloaded. So no need to restart all of kodi when changing the keymap(.xml), just restart the addon
+When you start the addon the keymap of kodi gets reloaded. So no need to restart all of kodi when changing the keymap(.xml), just restart the addon. 
 
 ## Known issue
-For some reason Home Assistant disconnects when you fire to may, automations at ones. and it response to that with a disconnect, i do not know a better way to then to just reconnect. 
+Possibly fixed in 0.3.0, let me know. For some reason Home Assistant disconnects when you fire to may, automations at ones. And it response to that with a disconnect, i do not know a better way to then to just reconnect. 
 
-# example
-In webos_example_automation.yaml there is a example of the automations that switches the remote between kodi(at the HDMI) and the webos apps. This way you can control the tv with one remote. For example a Measy GP811 or a RII usb wireless keyboard. In webos_example_keymap.xml is the kodi keymap to use with that.
+# Example
+This is a example how I use it:
+```mermaid
+flowchart LR
+    A["Remote"] -->|usb| B(Kodi)
+    B --> |NotifyAll| C[Kodi2Home]
+    D --> Z
+    D --> B
+    subgraph TV
+    direction RL
+    B
+    Z[Smart-TV]
+    end
+    subgraph HASSIO
+    direction BT
+    C -->|trigger automation| D{Home Assistant}
+    D
+    end
+```
+The remote controls the Smart TV or Kodi depending on withs the tv gets is source from. This way you control Smart-TV apps (like Netfix) and Kodi with the same remote and the same buttons. 
+In webos_example_automation.yaml is the example of the automations that switches the remote between kodi(at the HDMI) and the Smart-TV apps. This way you can control the tv with one remote. In webos_example_keymap.xml is the kodi keymap to use with a Measy remote.
+![Measy remote](doc/Measy-remote.jpeg)
 
-Tip's, commands or spelling error's, just submit an issue
+Tip's, commands or spelling error's; just submit an issue.
--- kodi2home/kodi2home.py
+++ kodi2home/kodi2home.py
@@ -1,5 +1,6 @@
 import asyncio
 from pykodi import get_kodi_connection, Kodi, CannotConnectError, InvalidAuthError
+
 # from jsonrpc_websocket import Server
 import signal
 import functools
@@ -8,140 +9,192 @@ import json
 import sys
 
 import logging
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, stream=sys.stdout)
-#put this in gen.xml
+
+logging.basicConfig(
+    format="%(asctime)s %(message)s", level=logging.INFO, stream=sys.stdout
+)
+# put this in gen.xml
 # <volume_up>NotifyAll("kodi2home", "kodi_call_home", {"trigger":"automation.volume_up"})</volume_up>
 
-loop = asyncio.get_event_loop()
-
-def ask_exit(signame):
-	print("got signal %s: exit" % signame)
-	loop.stop()
 
 
-class kodi2home():
-	id_nr = 1
-	
-	def __init__(self, config_file):
-		with open(config_file, 'r') as inputfile:
-			self.config = json.load(inputfile)
-		pass
-	
-	
-	async def connect_to_kodi(self):
-		logging.info(f"kodi settings: {self.config['kodi_adress']},  {self.config['kodi_http_port']} {self.config['kodi_ws_port']}, {self.config['kodi_username']}, , {self.config['kodi_password']}")
-		self.kodi_connection = get_kodi_connection(self.config['kodi_adress'], self.config['kodi_http_port'], self.config['kodi_ws_port'], self.config['kodi_username'], self.config['kodi_password'], False, 5, None)
-		await self.kodi_connection.connect()
-		self.kodi = Kodi(self.kodi_connection )
 
-		self.kodi_connection.server.Other.kodi_call_home = self.kodi_call_home
-		
-		properties = await self.kodi.get_application_properties(["name", "version"])
-		logging.info(f"Kodi is connected {properties}")
-		await self.kodi.ping()
-		await self.kodi_connection.server.Input.ExecuteAction("reloadkeymaps")
-
-	
-	async def connect_to_home(self):
-		logging.info(f"kodi settings: {self.config['home_adress']},  {self.config['home_ssl']}, {sys.argv[1]}")
-		if self.config['home_ssl']:
-			home_ssl = True
-		else:
-			home_ssl = None
-		self.websocket = await websockets.connect(self.config['home_adress'], ssl=home_ssl)
-		login_org = {"type": "auth", "access_token": sys.argv[1] }
-		data = await self.websocket.recv()
-		logging.info(f"Home Assistant: {data}")
-		await self.websocket.send(json.dumps(login_org))
-		data = await self.websocket.recv()
-		logging.info(f"Home Assistant: {data}")
-
-	
-	async def kodi_call_home(self, sender, data):
-		if "trigger" not in data:
-			return
-		
-		self.id_nr += 1;
-		service_call = {
-				  "id": self.id_nr,
-				  "type": "call_service",
-				  "domain": "automation",
-				  "service": "trigger",
-				  "service_data": {
-				    "entity_id": data["trigger"],
-				  }
-			  }
-		
-		logging.info(f"{sender} is calling home {json.dumps(service_call)}")
-		try:
-			await self.websocket.send(json.dumps( service_call ))
-		except (websockets.exceptions.ConnectionClosedOK):
-			await self.websocket.close()
-			await self.connect_to_home()
-			await self.websocket.send(json.dumps( service_call ))
-		except (websockets.exceptions.ConnectionClosedError):
-			await self.websocket.close()
-			await self.connect_to_home()
-			logging.info(f"reconnect on way to fast pressing buttons")
-			await self.websocket.send(json.dumps( service_call ))
-	
-	async def run(self):
-		
-		try:
-			await self.connect_to_kodi()
-		except ( CannotConnectError ):
-			logging.error("Reconnecting failed, try again")
-		except ( InvalidAuthError ):
-			logging.error("InvalidAuthError, wrong login")
-			return
-		
-		await self.connect_to_home()
-		logging.info("connected")
-		while 1:
-			try:
-				if self.kodi_connection.connected:
-					await self.kodi.ping()
-				else:
-					await self.connect_to_kodi()
-			except ( CannotConnectError ):
-				logging.error("Reconnecting failed, try again")
-			except ( InvalidAuthError ):
-				logging.error("InvalidAuthError, wrong login")
-				return
-			await asyncio.sleep( 10 )
-		logging.info("Done")
+def ask_exit(signame, loop):
+    print("got signal %s: exit" % signame)
+    loop.stop()
 
 
+class kodi2home:
+    id_nr = 1
+
+    def __init__(self, config_file):
+        with open(config_file, "r") as inputfile:
+            self.config = json.load(inputfile)
+        self.que1 = asyncio.Queue(maxsize=10)
+
+    async def connect_to_kodi(self):
+        logging.info(
+            f"kodi settings: {self.config['kodi_adress']},  {self.config['kodi_http_port']} {self.config['kodi_ws_port']}, {self.config['kodi_username']}, , {self.config['kodi_password']}"
+        )
+        self.kodi_connection = get_kodi_connection(
+            self.config["kodi_adress"],
+            self.config["kodi_http_port"],
+            self.config["kodi_ws_port"],
+            self.config["kodi_username"],
+            self.config["kodi_password"],
+            False,
+            5,
+            None,
+        )
+        await self.kodi_connection.connect()
+        self.kodi = Kodi(self.kodi_connection)
+
+        self.kodi_connection.server.Other.kodi_call_home = self.kodi_call_home
+
+        properties = await self.kodi.get_application_properties(["name", "version"])
+        logging.info(f"Kodi is connected {properties}")
+        await self.kodi_connection.server.Input.ExecuteAction("reloadkeymaps")
+
+    async def connect_to_home(self):
+        logging.info(
+            f"Home assistant settings: {self.config['home_adress']},  {self.config['home_ssl']}, {sys.argv[1]}"
+        )
+        if self.config["home_ssl"]:
+            home_ssl = True
+        else:
+            home_ssl = None
+        self.websocket = await websockets.connect(
+            self.config["home_adress"], ssl=home_ssl
+        )
+        login_org = {"type": "auth", "access_token": sys.argv[1]}
+        data = await self.websocket.recv()
+        logging.info(f"Home Assistant: {data}")
+        await self.websocket.send(json.dumps(login_org))
+        data = await self.websocket.recv()
+        logging.info(f"Home Assistant: {data}")
+
+    async def kodi_call_home(self, sender, data):
+        if "trigger" not in data:
+            return
+
+        self.id_nr += 1
+        service_call = {
+            "id": self.id_nr,
+            "type": "call_service",
+            "domain": "automation",
+            "service": "trigger",
+            "service_data": {
+                "entity_id": data["trigger"],
+            },
+        }
+
+        logging.info(f"{sender} is calling home {json.dumps(service_call)}")
+        self.que1.put_nowait(service_call)
+
+
+    async def run_send_home(self):
+        while 1:
+            service_call = await self.que1.get()
+            try:
+                await self.websocket.send(json.dumps(service_call))
+            except websockets.exceptions.ConnectionClosedOK:
+                await self.websocket.close()
+                await self.connect_to_home()
+                logging.info(f"reconnect on oke connection close")
+                await self.websocket.send(json.dumps(service_call))
+            except websockets.exceptions.ConnectionClosedError:
+                await self.websocket.close()
+                await self.connect_to_home()
+                logging.info(f"reconnect on way to fast pressing buttons")
+                await self.websocket.send(json.dumps(service_call))
+
+    async def run_recv_home(self):
+        await self.connect_to_home()
+        logging.info("connected to home")
+        while 1:
+            try:
+                async with asyncio.timeout(100):
+                    home_data = await self.websocket.recv()
+            except TimeoutError:
+                home_data = ""
+
+            if home_data != "":
+                logging.info(f"data from home: {home_data}")
+                message_home = json.loads(home_data)
+
+                if "type" in message_home:
+                    if message_home["type"] == "ping":
+                        pong_message = {
+                            "type": "pong",
+                            "id": message_home["id"]
+                        }
+                        await self.websocket.send(json.dumps(pong_message))
+            else:
+                self.id_nr += 1
+                ping_message = {
+                    "id": self.id_nr,
+                    "type": "ping"
+                }
+                await self.websocket.send(json.dumps(ping_message))
+            
+
+        
+
+    async def run_recive_kodi(self):
+        try:
+            await self.connect_to_kodi()
+        except CannotConnectError:
+            logging.error("Reconnecting failed, try again")
+        except InvalidAuthError:
+            logging.error("InvalidAuthError, wrong login")
+            return
+
+        while 1:
+            try:
+                if self.kodi_connection.connected:
+                    await self.kodi.ping()
+                else:
+                    await self.connect_to_kodi()
+            except CannotConnectError:
+                logging.error("Reconnecting failed, try again")
+            except InvalidAuthError:
+                logging.error("InvalidAuthError, wrong login")
+                return
+            await asyncio.sleep(100)
+
+
+async def add_handels():
+    loop = asyncio.get_running_loop()
+    for signame in ("SIGINT", "SIGTERM"):
+        loop.add_signal_handler(
+            getattr(signal, signame), functools.partial(ask_exit, signame, loop)
+        )
 
 def main():
-	
-	
-	for signame in ('SIGINT', 'SIGTERM'):
-		loop.add_signal_handler(getattr(signal, signame), functools.partial(ask_exit, signame))
-	
-	try:
-		k = kodi2home("options.json")
-		loop.run_until_complete(k.run())
-		loop.run_forever()
-	finally:
-		loop.close()
-	
-	
+    try:
+        k = kodi2home("options.json")
+        tasks = asyncio.gather(k.run_recive_kodi(), add_handels(), k.run_send_home(), k.run_recv_home())
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete( tasks )
+    finally:
+        loop = asyncio.get_running_loop()
+        loop.close()
+
+
 if __name__ == "__main__":
-	main()
-	
+    main()
+
 # 	loop.run_until_complete()
-	
 
-# ref: 
-#	https://github.com/lijinqiu1/homeassistant/blob/master/websocketAPI.py
-#	https://developers.home-assistant.io/docs/api/websocket#calling-a-service
-#	https://forums.homeseer.com/forum/media-plug-ins/media-discussion/kodi-xbmc-spud/76271-how-to-trigger-hs-events-from-kodi-xbmc-interface
-#	https://www.home-assistant.io/integrations/kodi/
-#	https://github.com/home-assistant/core/blob/dev/homeassistant/components/kodi/media_player.py
-#	https://github.com/OnFreund/PyKodi/blob/master/pykodi/kodi.py
-#	https://github.com/emlove/jsonrpc-websocket
-#	https://kodi.wiki/view/Keymap#Add-on_built-in.27s
-#	https://github.com/xbmc/xbmc/blob/master/system/keymaps/keyboard.xml
-#	https://github.com/home-assistant/core/blob/dev/homeassistant/components/kodi/__init__.py
 
+# ref:
+# 	https://github.com/lijinqiu1/homeassistant/blob/master/websocketAPI.py
+# 	https://developers.home-assistant.io/docs/api/websocket#calling-a-service
+# 	https://forums.homeseer.com/forum/media-plug-ins/media-discussion/kodi-xbmc-spud/76271-how-to-trigger-hs-events-from-kodi-xbmc-interface
+# 	https://www.home-assistant.io/integrations/kodi/
+# 	https://github.com/home-assistant/core/blob/dev/homeassistant/components/kodi/media_player.py
+# 	https://github.com/OnFreund/PyKodi/blob/master/pykodi/kodi.py
+# 	https://github.com/emlove/jsonrpc-websocket
+# 	https://kodi.wiki/view/Keymap#Add-on_built-in.27s
+# 	https://github.com/xbmc/xbmc/blob/master/system/keymaps/keyboard.xml
+# 	https://github.com/home-assistant/core/blob/dev/homeassistant/components/kodi/__init__.py
